55ff220bb3fdaaaf2fb4fcc2ab2209dfbc95b9d1
kuaikuai <yuxiaofeng3@YUXIAOFENG3.Hisense.ad>
2015-04-24 10:34:00 +0800

    ok

--- lua/waf_exclude.lua
+++ lua/waf_exclude.lua
@@ -5,7 +5,7 @@ M.args = { ["/cgi-bin/epg_index.fcgi"]={'picinfo','channelidinfo'},
            ["/api/file/upload_meta"]={"metaData"},
            ["/aaa/signon"]={"loginName"}
          }
-local reg = "^/sdkproxy/sendsms|^/log/report_terminal|^/log/report_exception"
+local reg = "^/sdkproxy/sendsms|^/log"
 
 function M.is_url_excluded(url)
    local v = ngx.re.match(url, reg, 'jo')
